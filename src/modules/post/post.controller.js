197c21b63e4555a3b655afc3365630d34413b5a4
Lester <lester.ung@pathmazing.com>
2018-04-03 21:55:52 +0700

    add post detail and update post end point

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -73,8 +73,8 @@ export const postList = async (req, res) => {
 };
 
 export const deletePost = async (req, res) => {
-  const { id } = req.params;
   try {
+    const { id } = req.params;
     const post = await Post.findByIdAndRemove(id);
 
     if (!post) {
@@ -92,7 +92,82 @@ export const deletePost = async (req, res) => {
       data: post,
       errors: null
     });
-  } catch(err) {
+  } catch (err) {
+    return res.status(500).json({
+      status: 'error',
+      data: null,
+      errors: String(err)
+    });
+  }
+};
+
+export const postDetail = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+
+    if (!post) {
+      return res.status(404).json({
+        status: 'error',
+        data: null,
+        errors: {
+          message: 'Post not found.'
+        }
+      });
+    }
+
+    return res.json({
+      status: 'success',
+      data: post,
+      errors: null
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 'error',
+      data: null,
+      errors: String(err)
+    });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  req.assert('title', 'Title is required.').exists();
+  req.assert('link', 'Link is required.').exists();
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    return res.status(412).json({
+      status: 'error',
+      data: null,
+      errors
+    });
+  }
+
+  try {
+    const { id } = req.params;
+    const { title, link } = req.body;
+
+    const post = await Post.findByIdAndUpdate(id, {
+      $set: { title, link }
+    });
+
+    if (!post) {
+      return res.status(404).json({
+        status: 'error',
+        data: null,
+        errors: {
+          message: 'Post not found.'
+        }
+      });
+    }
+
+    return res.json({
+      status: 'success',
+      data: post,
+      errors: null
+    });
+  } catch (err) {
     return res.status(500).json({
       status: 'error',
       data: null,
--- src/modules/post/post.routes.js
+++ src/modules/post/post.routes.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
-import { createPost, postList, deletePost } from './post.controller';
+import {
+  createPost,
+  postList,
+  deletePost,
+  postDetail,
+  updatePost
+} from './post.controller';
 
 const router = Router();
 
@@ -8,5 +14,9 @@ router.post('/create', createPost);
 router.get('/list', postList);
 
 router.delete('/delete/:id', deletePost);
+
+router.get('/:id', postDetail);
+
+routes.patch('/update/:id', updatePost);
 
 export default router;
